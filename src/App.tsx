af2ab09fbda12e4216a3edfa0880314b07895ad8
flor3z <lie9730@gmail.com>
2025-10-29 20:42:57 +0900

    [feat] add authentication routes to App
    인증 관련 페이지들의 라우트를 추가했습니다.
    로그인, 마이페이지, 로그아웃 기능에 접근할 수 있도록
    React Router 설정에 세 개의 새로운 경로를 통합했습니다.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from '@/pages/MainPage';
 import Home from '@/pages/Home';
 import ApiDemo from '@/pages/ApiDemo';
+import LoginPage from '@/pages/LoginPage';
+import MyPage from '@/pages/MyPage';
+import LogoutPage from '@/pages/LogoutPage';
 
 // 2. Component
 export default function App() {
@@ -13,6 +16,9 @@ export default function App() {
         <Route path="/" element={<MainPage />} />
         <Route path="/old-home" element={<Home />} />
         <Route path="/apidemo" element={<ApiDemo />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/logout" element={<LogoutPage />} />
       </Routes>
     </BrowserRouter>
   );
